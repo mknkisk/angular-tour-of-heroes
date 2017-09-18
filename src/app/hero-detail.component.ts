b4acd83e503a338efd0f275d266696b3daa25225
Keisuke Makino <mknkisk@gmail.com>
2017-09-18 16:33:29 +0900

    The hero property is an input property

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Hero } from './hero';
 
@@ -16,5 +16,5 @@ import { Hero } from './hero';
   `
 })
 export class HeroDetailComponent {
-  hero: Hero;
+  @Input() hero: Hero;
 }
